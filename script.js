b31e7d557eb5dbe8fb7fdad6e234506a5f847f9a
Kadeem Bowen <kadeem.bowen@ndma.gov.gy>
2024-02-16 09:35:26 -0400

    correct last push

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function calculateMortgage() {
-    const loanAmount = parseFloat(document.getElementById('loanAmount').value);
-    const interestRate = parseFloat(document.getElementById('interestRate').value) / 100 / 12;
-    const loanTerm = parseFloat(document.getElementById('loanTerm').value) * 12;
-  
-    const monthlyPayment = (loanAmount * interestRate) / (1 - Math.pow(1 + interestRate, -loanTerm));
-  
-    if (!isNaN(monthlyPayment) && (monthlyPayment !== Infinity) && (monthlyPayment !== -Infinity)) {
-      document.getElementById('monthlyPayment').textContent = `$${monthlyPayment.toFixed(2)}`;
-    } else {
-      document.getElementById('monthlyPayment').textContent = 'Invalid input. Please check your values.';
-    }
+  const loanAmount = parseFloat(document.getElementById('loanAmount').value);
+  const interestRate = parseFloat(document.getElementById('interestRate').value) / 100 / 12;
+  const loanTerm = parseInt(document.getElementById('loanTerm').value) * 12;
+
+  const monthlyPayment = (loanAmount * interestRate) / (1 - Math.pow(1 + interestRate, -loanTerm));
+
+  if (!isNaN(monthlyPayment) && (monthlyPayment !== Infinity) && (monthlyPayment !== -Infinity)) {
+    document.getElementById('monthlyPayment').textContent = `$${monthlyPayment.toFixed(2)}`;
+  } else {
+    document.getElementById('monthlyPayment').textContent = 'Invalid input. Please check your values.';
   }
+}
+
   
